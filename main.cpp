767f272c6fe94e0e7d96d3d928b93701fbe49b74
bulatsabirzyanov <sabirzyanov429@gmail.com>
2022-03-12 22:59:36 +0300

    hw1

--- main.cpp
+++ main.cpp
@@ -7,7 +7,16 @@ using namespace assignment;
 
 int main() {
 
-  cout << "Write your code here...\n";
-
-  return 0;
+  int task1=assignment::squared_diff(3,5);
+  cout<<task1;
+  int n;
+  cin>>n;
+  int mass[n];
+  int* point_on_mass = &mass[0];
+  for(int j = 0 ; j<n;j++)
+  {
+    cin>>mass[j];
+  }
+  int task5=assignment::arr_sum(point_on_mass,3);
+  cout<<task5;
 }
--- src/code.cpp
+++ src/code.cpp
@@ -3,74 +3,169 @@
 #include <iostream>   // cout, printf
 #include <algorithm>  // fill, copy
 
+
 namespace assignment {
 
   // Task 1
-  int squared_diff(int left, int right) {
-
-    // Write your code here ...
-
-    return 0;
+  int squared_diff(int left, int right)
+  {
+    return ((left - right )*(left - right ));
   }
 
   // Task 2
-  bool check_bit(int mask, int bit_pos) {
-
-    // Write your code here ...
-
-    return false;
+  bool check_bit(int mask, int bit_pos)
+  {
+    if(mask<=0 || bit_pos<=0)
+      {
+        return false;
+      }
+    else {
+      int mask2 = mask;
+      int count = 0;
+      int mass[100];
+      while (mask2 > 0)
+      {
+        mass[count] = (mask2 % 2);
+        mask2 /= 10;
+        count++;
+      }
+      for (int j = 0; j < count; j++) {
+        if (bit_pos == j) {
+          if (mass[j] == 1) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+    }
   }
 
   // Task 3
   int max3(int left, int middle, int right) {
-
-    // Write your code here ...
-
-    return 0;
+    int j=2;
+    int mass[3]={left,middle,right};
+    int max=0;
+    for (int i=0;i<=j;i++)
+    {
+      if (max<=mass[i])
+      {
+        max=mass[i];
+      }
+      else
+      {
+        max=max;
+      }
+    }
+    return max;
   }
 
   // Task 4
   void swap_args(int *left, int *right) {
 
-    // Write your code here ...
+    if(left != nullptr and right != nullptr){
+      int ptr = *left;
+      *left = *right;
+      *right = ptr;
+    }
   }
 
   // Task 5
   int arr_sum(int *arr, int length) {
-
-    // Write your code here ...
-
+  if(arr != nullptr and length>0) {
+    int sum = 0;
+    for (int i = 0; i < length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+  else
     return 0;
   }
 
   // Task 6
   int *find_max_elem(int *arr, int length) {
+    if(arr== nullptr or length<=0)
+    {
+      return nullptr;
+    }
+    else
+    {
+      int max=0;
+      int * id_max = nullptr;
+      for(int i =0;i<length;i++)
+      {
+        if(max<arr[i])
+            max=arr[i];
+            id_max=&arr[i];
 
-    // Write your code here ...
-
-    return nullptr;
+      }
+      return id_max;
+    }
   }
 
   // Task 7
-  int *allocate_arr(int length, int init_value) {
-
-    // Write your code here ...
-
-    return nullptr;
+  int *allocate_arr(int length, int init_value)
+  {
+    if(length > 0)
+      {
+      int arr[length];
+      for (int i = 0; i < length; i++)
+        {
+          arr[i] = init_value;
+        }
+        return arr;
+      }
+    else
+      return nullptr;
   }
 
   // Task 8
   int *clone_arr(int *arr_in, int length) {
 
-    // Write your code here ...
-
-    return nullptr;
+    if (length <=0 or arr_in == nullptr)
+      return nullptr;
+    else
+    {
+      int clone_arr[length];
+      for(int i =0; i<length;i++)
+      {
+        clone_arr[i] = arr_in[i];
+      }
+      return clone_arr;
+    }
   }
 
   // Task 9
   void print_kth_elements(int *arr, int length, int k, std::ostream &os) {
+    if (arr == nullptr)
+    {
+      os<< "Invalid argument: arr\n";
+    }
+    else
+    {
+      if (length <= 0)
+      {
+        os << "Invalid argument: length\n";
+      }
+      else
+      {
 
-    // Write your code here ...
+        if (k <= 0)
+        {
+          os << "Invalid argument: k\n";
+        }
+        else
+        {
+          for (int i=0;i<length;i+=k)
+          {
+            os << arr[i] << "\t";
+          }
+        }
+      }
+    }
+
   }
 
-}  // namespace assignment
+
+}
